b9cb17193ca8e48420f1e0b4f96ede50be745978
Everton <everton.rodrigues@rbxsoft.com>
2024-09-15 09:30:20 -0300

    Alterações nos arquivos com base nas aulas

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Criar novo usuário')
+
+@section('content')
+    @include('admin.users.partials.breadcrumb')
+    <div class="py-6">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Novo Usuário
+        </h2>
+    </div>
+
+{{-- @include('admin.includes.errors') --}}
+
+
+<form action=" {{ route('users.store') }} " method="POST">
+    @include('admin.users.partials.form')
+</form>
+@endsection
---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,25 @@
+@if (session()->has('success'))
+    <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if (session()->has('message'))
+    <div class="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800">
+        {{ session('message') }}
+    </div>
+@endif
+
+@if (session()->has('error'))
+    <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800">
+        {{ session('error') }}
+    </div>
+@endif
+
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li class="p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800">{{ $error }} </li>
+        @endforeach
+    </ul>
+@endif
